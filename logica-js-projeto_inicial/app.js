30e51bcb2db8b3e252048c5b45e1ba7004382ad2
vinicius <vinivinicius09@gmail.com>
2023-12-21 16:06:30 -0300

    Projeto atualizado e terminado

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,24 +1,23 @@
 alert("Bem vindo ao jogo do número secreto!")
-let numeroSecreto = 5;
+let numeroMaximo = parseInt(Math.random() * 10 + 1);//aqui eu gero um valor maximo aleatorio
+console.log(numeroMaximo);
+let numeroSecreto = parseInt(Math.random() * numeroMaximo +1);// o meu numéro secreto está entre 0 e o o numero máximo + 1 
+console.log(numeroSecreto);
 let chute;
-let tentativas = 2;
+let tentativas = 1;
 
 // laço de repetição que faz os loops enquanto o chute for dirente do numeroSecreto
 while (chute != numeroSecreto) {
-    chute = prompt("Digite seu número: ");
+    chute = prompt(`Digite um número entre 1 e ${numeroMaximo}.`);
     if (chute == numeroSecreto) {
-        alert(`Parabéns você acertou o número secreto (${numeroSecreto})!`);
-    } else { //esse trecho faz uma verificação do meu número de tentativas e da as dicas se chute é maior ou menor que o numeroSecreto
-        if (tentativas <= 0) {
-            alert("Você errou, acabram todas as suas tentativas, reinicie o jogo!!");
-            break;
+        break;
+    } else { // verifica o valor de chute com o do numero secreto
+        if (chute > numeroSecreto) {
+            alert(`O número secreto é menor que ${chute}`);
         } else {
-            tentativas--;
-            if (chute > numeroSecreto) {
-                alert(`O número secreto é menor que ${chute}`);
-            } else {
-                alert(`O número secreto é maior que ${chute}`)
-            }
+            alert(`O número secreto é maior que ${chute}`)
         }
-    }
-}//teste
+    } tentativas++;
+}
+let tenta = tentativas > 1 ? "tentativas!" : "tentativa!";
+alert(`Parabéns você acertou o número secreto (${numeroSecreto}) com ${tentativas} ${tenta}`);
